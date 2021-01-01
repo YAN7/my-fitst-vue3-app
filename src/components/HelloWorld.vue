f03c9d981f2bedf7d36a0a3e4349e3b68169c693
YAN7 <904394373@qq.com>
2021-01-01 17:16:08 +0800

    initital commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div class="box">
+    <div>hello</div>
+    <div>world</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      msg: 123123,
+    },
+  },
+};
+
+</script>
+
+<style lang="less" scoped>
+h1 {
+  color : green;
+}
+.box {
+  display: flex;
+}
+</style>
