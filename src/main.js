7e61631a37230906f15e8586c72de79adbd8b6a7
YAN7 <904394373@qq.com>
2021-01-04 23:14:00 +0800

    引入vue router4.0

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import './index.css';
 
-createApp(App).mount('#app')
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+});
+
+createApp(App)
+    .use(router)
+    .mount('#app');
---
+++ src/pages/Home/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      {{text}}
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Home',
+    setup() {
+        return {
+            text: 123,
+        };
+    },
+});
+</script>
+
+<style scoped lang="less">
+
+</style>
